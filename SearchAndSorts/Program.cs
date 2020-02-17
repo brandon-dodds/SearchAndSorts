5543816b0f71fb51b5e2e01fad6593e5613ba9ee
Brandon-Lee Dodds <brandon.lee.dodds@gmail.com>
2020-02-17 22:40:05 +0000

    Add project files.

---
+++ SearchAndSorts/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SearchAndSorts
+{
+    class Program
+    {
+        static void printArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 7, 3, 5, 1 };
+            Sorts.insertionSort(array);
+            printArray(array);
+        }
+    }
+}
---
+++ SearchAndSorts/Sorts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchAndSorts
+{
+    class Sorts
+    {
+        public static void bubblesort(int[] x)
+        {
+            for(int i = 0; i < x.Length - 1; i++)
+            {
+                for(int j = 0; j < x.Length - 1; j++)
+                {
+                    int temp;
+                    if(x[j] > x[j + 1])
+                    {
+                        temp = x[j];
+                        x[j] = x[j + 1];
+                        x[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void insertionSort(int[] x)
+        {
+            for(int i = 1; i < x.Length; i++)
+            {
+                int temp;
+                int index = i;
+                while(x[index] < x[index - 1] && index > 0)
+                {
+                    temp = x[index - 1];
+                    x[index - 1] = x[index];
+                    x[index] = temp;
+                    index--;
+                }
+            }
+        }
+    }
+}

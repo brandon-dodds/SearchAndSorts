5b7ae8f18d880aa8cb0733f42430718397853814
Brandon-Lee Dodds <19696909@students.lincoln.ac.uk>
2020-02-28 13:02:45 +0000

    Added 2048 net.

--- SearchAndSorts/Program.cs
+++ SearchAndSorts/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-
+using System.Collections.Generic;
 namespace SearchAndSorts
 {
     class Program
@@ -10,16 +10,17 @@ namespace SearchAndSorts
             Console.Clear();
             var sortedArrayAsc = Sorts.MergeSort(array, "asc");
             var sortedArrayDesc = Sorts.MergeSort(array, "desc");
-            Console.WriteLine("Printing every 10th value ascending...");
-            for(int i = 0; i <= 250; i+=10)
+            Console.WriteLine((array.Length == 256) ? "Printing every 10th value ascending..." : "printing every 50th value ascending...");
+            var intUp = array.Length == 256 ? 10 : 50;
+            for(int i = 0; i <= (10 * (array.Length / 10)); i+=intUp)
             {
                 Console.WriteLine($"{i}. {sortedArrayAsc[i]}");
             }
             Console.WriteLine("Press a button to sort descending...");
             Console.ReadLine();
             Console.Clear();
-            Console.WriteLine("Printing every 10th value descending...");
-            for (int i = 0; i <= 250; i += 10)
+            Console.WriteLine((array.Length == 256) ? "Printing every 10th value descending..." : "printing every 50th value descending...");
+            for (int i = 0; i <= (10 * (array.Length / 10)); i += intUp)
             {
                 Console.WriteLine($"{i}. {sortedArrayDesc[i]}");
             }
@@ -37,14 +38,26 @@ namespace SearchAndSorts
             var net1String = File.ReadAllLines("Net_1_256.txt");
             var net2String = File.ReadAllLines("Net_2_256.txt");
             var net3String = File.ReadAllLines("Net_3_256.txt");
+            var net4String = File.ReadAllLines("Net_1_2048.txt");
+            var net5String = File.ReadAllLines("Net_2_2048.txt");
+            var net6String = File.ReadAllLines("Net_3_2048.txt");
             int[] net1int = new int[net1String.Length];
             int[] net2int = new int[net2String.Length];
             int[] net3int = new int[net3String.Length];
+            int[] net4int = new int[net4String.Length];
+            int[] net5int = new int[net5String.Length];
+            int[] net6int = new int[net6String.Length];
             for (int i = 0; i < 256; i++)
             {
                 net1int[i] = int.Parse(net1String[i]);
                 net2int[i] = int.Parse(net2String[i]);
                 net3int[i] = int.Parse(net3String[i]);
+            }
+            for (int i = 0; i < 2048; i++)
+            {
+                net4int[i] = int.Parse(net4String[i]);
+                net5int[i] = int.Parse(net5String[i]);
+                net6int[i] = int.Parse(net6String[i]);
             }
             while (true)
             {
@@ -52,7 +65,10 @@ namespace SearchAndSorts
                 Console.WriteLine("Select array to be analysed:" +
                     "\n1. Net_1_256.txt" +
                     "\n2. Net_2_256.txt" +
-                    "\n3. Net_3_256.txt");
+                    "\n3. Net_3_256.txt" +
+                    "\n4. Net_1_2048.txt" +
+                    "\n5. Net_2_2048.txt" +
+                    "\n6. Net_3_2048.txt");
                 var userChoice = Console.ReadLine();
                 switch (userChoice)
                 {
@@ -64,6 +80,15 @@ namespace SearchAndSorts
                         break;
                     case "3":
                         ArrayAnalysis(net3int);
+                        break;
+                    case "4":
+                        ArrayAnalysis(net4int);
+                        break;
+                    case "5":
+                        ArrayAnalysis(net5int);
+                        break;
+                    case "6":
+                        ArrayAnalysis(net6int);
                         break;
                     default:
                         Console.WriteLine("Please enter a correct value.");
